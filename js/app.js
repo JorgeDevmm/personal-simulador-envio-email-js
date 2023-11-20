021df2baf9db7d024a505345d4d28769cbbbbff0
JorgeDevmm <jorge29luis10@gmail.com>
2023-11-20 11:09:54 -0500

    Definiendo la referencia de los inputs

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// validamos que se descargo todo el codigo html
+document.addEventListener('DOMContentLoaded', () => {
+  // Seleccionar los elementos de la interfaz
+  const inputEmail = document.querySelector('#email');
+  const inputAsunto = document.querySelector('#asunto');
+  const inputMensaje = document.querySelector('#mensaje');
+
+  
+});
