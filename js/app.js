2a08ad0d2cde8cd2be108434314903ca96efa00d
JorgeDevmm <jorge29luis10@gmail.com>
2023-11-21 12:07:49 -0500

    Añadiendo un Spinner

--- js/app.js
+++ js/app.js
@@ -12,17 +12,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const inputMensaje = document.querySelector('#mensaje');
 
   const formulario = document.querySelector('#formulario');
+  const spinner = document.querySelector('#spinner');
 
   const btnSubmit = document.querySelector("#formulario button[type='submit']");
 
   const btnReset = document.querySelector("#formulario button[type='reset']");
 
-  // Asignar eventos
+  //ANCHOR Asignar eventos
 
   // evento al abandonar campo
   inputEmail.addEventListener('input', validar);
   inputAsunto.addEventListener('input', validar);
   inputMensaje.addEventListener('input', validar);
+
+  formulario.addEventListener('submit', enviarEmail);
+
   btnReset.addEventListener('click', function (e) {
     e.preventDefault();
 
@@ -31,9 +35,21 @@ document.addEventListener('DOMContentLoaded', () => {
       email[clave] = '';
     }
 
-    formulario.reset();//reseteo de campos visual del formulario
+    formulario.reset(); //reseteo de campos visual del formulario
     comprobarEmail();
   });
+
+  function enviarEmail(e) {
+    e.preventDefault();
+
+    if (spinner.classList.contains("hidden")) {
+      spinner.classList.add('flex');
+      spinner.classList.remove('hidden');
+      formulario.reset(); //reseteo de campos visual del formulario
+      comprobarEmail();
+      console.log('enviando..');
+    }
+  }
 
   // función validar
   function validar(e) {
